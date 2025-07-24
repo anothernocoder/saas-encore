94112b5c6b7b3461c0709bccc44bc2db61c4d588
Hector Suarez <hectorjaime15@gmail.com>
2025-07-24 00:14:23 -0500

    ✏️ Add the vercel url

--- backend/config.ts
+++ backend/config.ts
@@ -1,3 +1,3 @@
 // The base url for where the frontend is hosted.
 // Update to point to where you are hosting your frontend.
-export const FRONTEND_URL = "https://encorets-saas-starter.vercel.app";
+export const FRONTEND_URL = "saas-encore.vercel.app";
